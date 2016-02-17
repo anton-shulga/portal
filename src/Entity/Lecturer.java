3853935d757e70388132357dde6f740f43505da7
Wadim <wadimbylvar@gmail.com>
2016-02-17 04:58:19 +0300

    Added ierarchery

--- src/Entity/Lecturer.java
+++ src/Entity/Lecturer.java
@@ -4,4 +4,7 @@ package Entity;
  * Created by Антон on 16.02.2016.
  */
 public class Lecturer extends User {
+    public Lecturer(int id, String login, String name, String surname, String email, String phoneNumber, byte banStatus) {
+        super(id, login, name, surname, email, phoneNumber, banStatus);
+    }
 }
